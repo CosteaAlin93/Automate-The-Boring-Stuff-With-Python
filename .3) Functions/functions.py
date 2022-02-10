18fcff6b84a8a8bf10e907a8fedb82ab5f383720
CosteaAlin93 <costeaalin93@gmail.com>
2022-02-10 08:41:56 +0100

    Section3 + folder organization

---
+++ .3) Functions/functions.py
@@ -0,0 +1,10 @@
+def hello():
+    print('a')
+    print('b')
+    print('c')
+print('1st execution')    
+hello()
+print('2nd execution')   
+hello()
+print('3rd execution')   
+hello()    
---
+++ .3) Functions/printOnSameLineAndWithSeparators.py
@@ -0,0 +1,4 @@
+print('Hello ', end='')
+print('World')
+
+print('cat', 'dog', 'mouse', sep=' PAUSE ')
